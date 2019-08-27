e86b7cf6d59225aaf89d8019b321a2d1bcebb161
Ardakaniz <ardakaniz404@gmail.com>
2019-08-27 22:33:34 +0200

    [ucg] Implement exec lua function

--- ucode_generator/Engine.cpp
+++ ucode_generator/Engine.cpp
@@ -27,21 +27,29 @@ Engine::Engine(const std::string& script_folder) :
 		return true;
 	});
 
-	m_lua.set_function("exec", [](const sol::table& cs) {
-		unsigned int final_cs = 0;
-		for (auto i : cs) {
-			if (i.second.get_type() != sol::type::number)
-				throw std::runtime_error{ "[script.lua] exec(): Invalid arguments, expected control signal flag" };
-
-			final_cs |= i.second.as<unsigned int>();
-		}
-
-		std::cout << final_cs << std::endl;
-	});
+	
 }
 
 void Engine::generate() {
-	m_lua.script_file(m_script_folder + "/script.lua");
+	using namespace std::placeholders;
+
+	bool is_start{ false }, do_fetch{ false };
+	m_lua.set_function("exec", [&is_start, &do_fetch, this](const sol::table& cs) { lua_exec( is_start, do_fetch, true, true, true, cs); });
+	m_lua.set_function("exec_no_start", [&is_start, &do_fetch, this](const sol::table& cs) { lua_exec(is_start, do_fetch, false, true, true, cs); });
+	m_lua.set_function("exec_no_fetch", [&is_start, &do_fetch, this](const sol::table& cs) { lua_exec(is_start, do_fetch, true, false, true, cs); });
+	m_lua.set_function("exec_no_phase", [&is_start, &do_fetch, this](const sol::table& cs) { lua_exec(is_start, do_fetch, true, true, false, cs); });
+
+	sol::load_result script = m_lua.load_file(m_script_folder + "/script.lua");
+
+	for (m_rom_index = 0; m_rom_index < m_ucode_rom.size(); ++m_rom_index) {
+		is_start = true;
+		m_phase = 0;
+		if (auto result = script(); !result.valid())
+			sol::script_throw_on_error(m_lua.lua_state(), result);
+
+		if (do_fetch)
+			m_ucode_rom[m_rom_index] |= m_fetch_cycle;
+	}
 }
 
 void Engine::save_hex(const std::string& output_filename) {
@@ -50,11 +58,14 @@ void Engine::save_hex(const std::string& output_filename) {
 
 void Engine::generate_ctrl_addr() {
 	const auto flags = expect_value<std::vector<std::string>>("flags");
-	const auto phase_count = expect_value<unsigned int>("phase_count");
+	m_phase_count = expect_value<unsigned int>("phase_count");
 	const auto instructions = expect_value<std::vector<std::string>>("instructions");
 
 	const auto ctrl_addr_org = expect_value<std::vector<unsigned int>>("ctrl_addr_org");
+
 	std::vector<unsigned int> already_found{};
+	m_phase_pos = 0;
+	bool m_phase_found{ false };
 	for (auto i : ctrl_addr_org) {
 		if (std::find(std::begin(already_found), std::end(already_found), i) != std::end(already_found)) {
 			throw std::runtime_error{ "[setup.lua] Invalid control adress organization: cannot have multiple time the same flag" };
@@ -65,20 +76,29 @@ void Engine::generate_ctrl_addr() {
 			for (const auto& flag : flags) {
 				m_ctrl_addr[flag] = false;
 			}
+
+			if (!m_phase_found)
+				m_phase_pos += static_cast<unsigned int>(flags.size());
 			break;
 
 		case 1: // PHASE
-			for (unsigned int phase{ 0 }; phase < static_cast<unsigned int>(std::ceil(std::log2(phase_count))); ++phase) {
+			for (unsigned int phase{ 0 }; phase < static_cast<unsigned int>(std::ceil(std::log2(m_phase_count))); ++phase) {
 				m_ctrl_addr["p" + std::to_string(phase)] = false;
 			}
 
+			m_phase_found = true;
 			break;
 
 		case 2: // OPCODE
-			for (unsigned int opcode{ 0 }; opcode < static_cast<unsigned int>(std::ceil(std::log2(instructions.size()))); ++opcode) {
+		{
+			const unsigned int opcode_bits = static_cast<unsigned int>(std::ceil(std::log2(instructions.size())));
+			for (unsigned int opcode{ 0 }; opcode < opcode_bits; ++opcode) {
 				m_ctrl_addr["op" + std::to_string(opcode)] = false;
 			}
 
+			if (!m_phase_found)
+				m_phase_pos += opcode_bits;
+		}
 			break;
 
 		default:
@@ -91,13 +111,18 @@ void Engine::generate_ctrl_addr() {
 	if (already_found.size() != 3)
 		throw std::runtime_error{ "[setup.lua] Invalid control adress organization: FLAGS, PHASE or OPCODE is missing" };
 
+	m_phase_pos = m_ctrl_addr.size() - m_phase_pos - static_cast<unsigned int>(std::ceil(std::log2(m_phase_count))); // Because lsb first in Lua code
+
+	m_ucode_rom.resize(static_cast<std::size_t>(std::pow(2, m_ctrl_addr.size())), false);
+
 	update_ctrl_addr();
 }
 
 void Engine::generate_ctrl_sigs() {
 	const auto ctrl_sigs = expect_value<std::vector<std::string>>("ctrl_sigs");
+	m_ctrl_sigs_count = static_cast<unsigned int>(ctrl_sigs.size());
 
-	for (unsigned int i{ 0 }; i < ctrl_sigs.size(); ++i) {
+	for (unsigned int i{ 0 }; i < m_ctrl_sigs_count; ++i) {
 		update_lua_table(string_split<'.'>(ctrl_sigs[i]), static_cast<unsigned int>(std::pow(2, i)));
 	}
 
@@ -149,4 +174,40 @@ unsigned int Engine::get_ctrl_sequence(const std::string& lua_value) {
 	}
 
 	return ctrl_sequence;
+}
+
+void Engine::lua_exec(bool& is_start, bool& do_fetch, bool start_cycle, bool fetch_cycle, bool phase_inc, const sol::table& cs) {
+	if (m_phase >= m_phase_count)
+		throw std::runtime_error{ "Too many exec instruction: phase count is " + std::to_string(m_phase_count) };
+
+	const unsigned int phase_mask = (static_cast<unsigned int>(std::pow(2, std::ceil(std::log2(m_phase_count)))) - 1);
+	if (((m_rom_index >> m_phase_pos) & phase_mask) != m_phase) { // Not the right phase, we don't exec anything
+		do_fetch = false;
+		if (phase_inc)
+			++m_phase;
+
+		return;
+	}
+	
+	unsigned int final_cs = 0;
+	for (auto i : cs) {
+		if (i.second.get_type() != sol::type::number)
+			throw std::runtime_error{ "[script.lua] exec(): Invalid arguments, expected control signal flag" };
+
+		final_cs |= i.second.as<unsigned int>();
+	}
+
+	if (start_cycle && is_start) {
+		final_cs |= m_start_cycle;
+		
+		is_start = false;
+	}
+	if (phase_inc) {
+		final_cs |= m_phase_inc;
+		++m_phase;
+	}
+
+	m_ucode_rom[m_rom_index] = final_cs;
+
+	do_fetch = fetch_cycle;
 }
--- ucode_generator/Engine.hpp
+++ ucode_generator/Engine.hpp
@@ -23,16 +23,19 @@ private:
 
 	unsigned int get_ctrl_sequence(const std::string& lua_value);
 
+	void lua_exec(bool& is_start, bool& do_fetch, bool start_cycle, bool fetch_cycle, bool phase_inc, const sol::table& cs);
+	
 	template<typename T> void update_lua_table(const std::vector<std::string>& path, T value);
 	template<typename T> T expect_value(const std::string& name);
 
-
-
+	unsigned int m_ctrl_sigs_count{ 0 };
+	unsigned int m_phase{ 0 }, m_phase_count{ 0 }, m_phase_pos{ 0 };
+	unsigned int m_rom_index{ 0 };
+	unsigned int m_start_cycle{ 0 }, m_fetch_cycle{ 0 }, m_phase_inc{ 0 };
 	sol::state m_lua;
 	std::string m_script_folder{};
-
 	std::unordered_map<std::string, bool> m_ctrl_addr{};
-	unsigned int m_start_cycle{ 0 }, m_fetch_cycle{ 0 }, m_phase_inc{ 0 };
+	std::vector<unsigned int> m_ucode_rom{};
 };
 
 #include "Engine.inl"
