78089420a43ae8cbb330acc8637ce765328d91c6
Ardakaniz <ardakaniz404@gmail.com>
2019-08-28 20:19:18 +0200

    [ucg] Update setup script according to architecture

--- ucode_generator/scripts/setup.lua
+++ ucode_generator/scripts/setup.lua
@@ -1,22 +1,38 @@
-flags = { "slf", "elf", "acf", "bzh", "rf", "lff", "bzf" }
+flags = { "rf", "bzf", "acf", "elf", "slf", "lff" }
 phase_count = 3
 instructions = { "+", "-", ">", "<", "[", "]", ".", "," }
 
 ctrl_addr_org = { FLAGS, PHASE, OPCODE }
 
 ctrl_sigs = {
+	"FLAGS.SLS",
+	"FLAGS.ELS",
+	"FLAGS.LSR",
+	"FLAGS.BZL",
+	"PHASE.CO",
+	"PHASE.RESET",
+	"PC.LO",
+	"PC.CO",
+	"PC.UP",
+	"MAR.RESET",
+	"MAR.CO",
+	"MAR.UP",
+	"LAR.LO",
+	"LAR.UP",
+	"LPC.RESET",
+	"LPC.CO",
+	"LPC.UP",
+	"RAMMULT.S0",
+	"RAM.WE",
+	"RAM.OE",
+	"BUSMULT.S0",
+	"BUSMULT.S1",
+	"BUSMULT.OE",
 	"ALU.LO",
 	"ALU.CO",
 	"ALU.UP",
-	"PHASE.CO",
-	"PHASE.RESET",
-	"PC.CO",
-	"PC.UP",
-	"LAR.LO",
-	"MAR.CO",
-	"MAR.UP",
-	"RAM.OE",
-	"RAM.WE",
+	"IO.PA",
+	"IO.PB",
 }
 
 start_cycle = { "LAR.LO" }
